e9e62cf442acca72647090ed2c63d5cfc76b07f1
André Oliveira <castrodeko11@hotmail.com>
2020-01-26 21:04:35 -0300

    Correção do Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -22,7 +22,7 @@ export default function Login({ history }){
         <>
         
         <p> 
-        Ofereça <strong>spots</strong> para os programadres e encontre <strong>talentos</strong> para sua empresa
+        Ofereça <strong>spots</strong> para os programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
 
       <form onSubmit={handleSubmit}>
